1569b9e68c0c66a48c9db9ccedabf09ae0fa9e7a
lewis-king <lking1221@gmail.com>
2018-05-12 22:04:09 +0100

    Handle cases when a repo does not have any active open pull requests

--- service/pullRequestService.js
+++ service/pullRequestService.js
@@ -3,13 +3,14 @@ const repos = require('../config/repos.json');
 const Constants = require('../config/constants');
 
 function retrieveOpenPullRequests(callback) {
-    const openPullRequestResponses = [];
+    let openPullRequestResponses = [];
     repos.forEach(repo => {
         const reqURL = Constants.bitbucketAPIContextRoot + repo.project + '/' + repo.repo + Constants.pullRequestAPIPathSuffix;
         request(reqURL, function (error, response, body) {
             let openPullRequests = JSON.parse(body);
             openPullRequestResponses.push(openPullRequests);
             if (openPullRequestResponses.length === repos.length) {
+                openPullRequestResponses = openPullRequestResponses.filter(item => item.size !== 0);
                 callback(openPullRequestResponses);
             }
         });
