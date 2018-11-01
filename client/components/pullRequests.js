48297f7e353a8923190eb4596cbad910c7ac70db
james <jamessykes94@gmail.com>
2018-11-01 15:38:56 +0000

    Addressed PR comments

--- client/components/pullRequests.js
+++ client/components/pullRequests.js
@@ -4,16 +4,17 @@ import {fetchOpenPullRequests} from "../actions/index";
 
 class PullRequest extends Component {
 
-
     componentDidMount() {
         console.log("In component did mount");
         this.props.fetchOpenPullRequests();
+    }
 
+    calculateHours(date){
+        return parseInt(((new Date() - new Date(date)) / 3600000).toFixed(0));
     }
 
     render() {
         const dateOptions = {year: 'numeric', month: 'short', day: 'numeric'};
-
         const openPullRequests = this.props.openPullRequests;
         if (!openPullRequests) {
             return (
@@ -36,7 +37,7 @@ class PullRequest extends Component {
                             Repository: {openPullRequest.values[0].destination.repository.name}
                             <br/>
                         </div>
-                        <table id='tableID' key={i}>
+                        <table key={i}>
                             <tbody>
                             <tr key={i}>
                                 <th>Title</th>
@@ -50,9 +51,8 @@ class PullRequest extends Component {
                                     <td class="PRTitle">{openPR.title}</td>
                                     <td class="author">{openPR.author.display_name}</td>
                                     <td class="created">{new Date(openPR.created_on).toLocaleDateString('en-GB', dateOptions)}</td>
-                                    <td className={parseInt(((new Date() - new Date(openPR.created_on)) / 3600000).toFixed(0)) > 24 ? "red" :
-                                    (parseInt(((new Date() - new Date(openPR.created_on)) / 3600000).toFixed(0)) < 6 ? "green" : "yellow")}>
-                                    {((new Date() - new Date(openPR.created_on)) / 3600000).toFixed(0)}</td>
+                                    <td className={(this.calculateHours(openPR.created_on) > 24 ? "red" : this.calculateHours(openPR.created_on) < 6 ? "green" : "yellow")}>
+                                    {(this.calculateHours(openPR.created_on))}</td>
                                     <td class="link"><a href={openPR.links.html.href}>Open Pull Request</a></td>
                                 </tr>
                             ))}
